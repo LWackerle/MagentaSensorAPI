f81f3e13a56799cbbcfc9e66e359aa20e2e9a2c3
Max Henke <admin@mxh.io>
2020-10-29 23:20:22 +0100

    debug console.log

--- api/src/services/devices/devices.class.js
+++ api/src/services/devices/devices.class.js
@@ -2,6 +2,7 @@ const { Service } = require("feathers-mongoose");
 
 exports.Devices = class Devices extends Service {
   async create(data, params) {
+    console.log(data);
     const docs = super.find({ device_id: data.service_id });
     if (!docs || docs.length === 0) {
       return super.create(data, params);
