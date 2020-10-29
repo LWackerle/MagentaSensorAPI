672b9744bad8cd9daa6c92e67d3c96eeaf29dc20
Max Henke <admin@mxh.io>
2020-10-29 23:25:55 +0100

    fixed query

--- api/src/services/devices/devices.class.js
+++ api/src/services/devices/devices.class.js
@@ -3,7 +3,7 @@ const { Service } = require("feathers-mongoose");
 exports.Devices = class Devices extends Service {
   async create(data, params) {
     if (data && data.device_id) {
-      const docs = super.find({ device_id: data.device_id });
+      const docs = super.find({ query: { device_id: data.device_id } });
       if (!docs || docs.length === 0) {
         return super.create(data, params);
       } else {
