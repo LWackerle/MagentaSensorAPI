83382f68bc43fd3e949f19c36b80f7188e042d75
Max Henke <admin@mxh.io>
2020-10-29 23:24:11 +0100

    fixed wrong variable name

--- api/src/services/devices/devices.class.js
+++ api/src/services/devices/devices.class.js
@@ -2,12 +2,13 @@ const { Service } = require("feathers-mongoose");
 
 exports.Devices = class Devices extends Service {
   async create(data, params) {
-    console.log(data);
-    const docs = super.find({ device_id: data.service_id });
-    if (!docs || docs.length === 0) {
-      return super.create(data, params);
-    } else {
-      return super.patch(data.service_id, data);
+    if (data && data.device_id) {
+      const docs = super.find({ device_id: data.device_id });
+      if (!docs || docs.length === 0) {
+        return super.create(data, params);
+      } else {
+        return super.patch(data.service_id, data);
+      }
     }
   }
 };
