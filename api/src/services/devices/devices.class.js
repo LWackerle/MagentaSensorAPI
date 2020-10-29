25be653a419653a81c2b3d4703541ca4168a036c
Max Henke <admin@mxh.io>
2020-10-29 23:27:12 +0100

    fixed wrong variable name

--- api/src/services/devices/devices.class.js
+++ api/src/services/devices/devices.class.js
@@ -4,10 +4,11 @@ exports.Devices = class Devices extends Service {
   async create(data, params) {
     if (data && data.device_id) {
       const docs = super.find({ query: { device_id: data.device_id } });
+      console.log(docs);
       if (!docs || docs.length === 0) {
         return super.create(data, params);
       } else {
-        return super.patch(data.service_id, data);
+        return super.patch(data.device_id, data);
       }
     }
   }
