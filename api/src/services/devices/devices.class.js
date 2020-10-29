e12d98448d75f46b79925be2a7129933167c0d9c
Max Henke <admin@mxh.io>
2020-10-29 23:31:46 +0100

    added missing paging

--- api/src/services/devices/devices.class.js
+++ api/src/services/devices/devices.class.js
@@ -3,9 +3,8 @@ const { Service } = require("feathers-mongoose");
 exports.Devices = class Devices extends Service {
   async create(data, params) {
     if (data && data.device_id) {
-      const docs = await super.find({ query: { device_id: data.device_id } });
-      console.log(docs);
-      if (!docs || docs.length === 0) {
+      const req = await super.find({ query: { device_id: data.device_id } });
+      if (!req || !req.data || req.data.length === 0) {
         return super.create(data, params);
       } else {
         return super.patch(data.device_id, data);
