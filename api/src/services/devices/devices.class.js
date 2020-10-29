4b4e950d520c7384c444c9ed185e085aeeb96f91
Max Henke <admin@mxh.io>
2020-10-29 23:28:56 +0100

    added missing async

--- api/src/services/devices/devices.class.js
+++ api/src/services/devices/devices.class.js
@@ -3,7 +3,7 @@ const { Service } = require("feathers-mongoose");
 exports.Devices = class Devices extends Service {
   async create(data, params) {
     if (data && data.device_id) {
-      const docs = super.find({ query: { device_id: data.device_id } });
+      const docs = await super.find({ query: { device_id: data.device_id } });
       console.log(docs);
       if (!docs || docs.length === 0) {
         return super.create(data, params);
