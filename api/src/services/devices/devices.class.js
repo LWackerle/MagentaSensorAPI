456344e9ed987665c18c28e9fa07efcc29486727
Max Henke <admin@mxh.io>
2020-10-29 23:17:48 +0100

    ajusted post method

--- api/src/services/devices/devices.class.js
+++ api/src/services/devices/devices.class.js
@@ -2,11 +2,11 @@ const { Service } = require("feathers-mongoose");
 
 exports.Devices = class Devices extends Service {
   async create(data, params) {
-    const doc = super.get(data.service_id);
-    if (doc) {
-      return super.patch(data.service_id, data);
-    } else {
+    const docs = super.find({ device_id: data.service_id });
+    if (!docs || docs.length === 0) {
       return super.create(data, params);
+    } else {
+      return super.patch(data.service_id, data);
     }
   }
 };
